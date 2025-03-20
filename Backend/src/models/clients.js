cf8c9899a28eeace63816cea80d48e5547f7832b
Jaim3w <20200147@ricaldone.edu.sv>
2025-03-20 11:25:31 -0600

    cambios

---
+++ Backend/src/models/clients.js
@@ -0,0 +1,49 @@
+/*
+clients
+
+idClient
+name
+Lastname
+email
+password
+birthDate
+Phone
+ProfilePicture
+*/
+
+
+
+
+import { Schema } from "mongoose";
+
+const clientsSchema = new Schema(
+{
+
+    name: {
+        type: String,
+        require: true,
+        maxHeight:100 ,
+      },
+      lastName: {
+        type: String,
+        require: true,
+        maxHeight:100,
+      }, 
+      birthday:{
+        type: Date,
+        require: true,
+        maxHeight: 50,
+      },
+      email:{
+        type:String,
+        require:true,
+        maxHeight: 100,
+        
+      },
+      password:{
+        type:String,
+        require:true,
+      },
+}
+
+)
